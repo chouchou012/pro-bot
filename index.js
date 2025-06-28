3af19768ea875d0641a8c3c3d1ac2541b65ad6db
chouchou012 <braboskhoury@gmail.com>
2025-06-28 11:23:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -139,10 +139,10 @@ function startBotForUser(chatId, config) { // <--- تم نقلها هنا لتك
                             const previousCandleClose = currentTickPrice;
 
                             if (previousCandleClose < previousCandleOpen) {
-                                tradeDirection = 'CALL';
+                                tradeDirection = 'PUT';
                                 bot.sendMessage(chatId, `📉 الشمعة السابقة (5 دقائق) هابطة (فتح: ${previousCandleOpen.toFixed(3)}, إغلاق: ${previousCandleClose.toFixed(3)}).`);
                             } else if (previousCandleClose > previousCandleOpen) {
-                                tradeDirection = 'PUT';
+                                tradeDirection = 'CALL';
                                 bot.sendMessage(chatId, `📈 الشمعة السابقة (5 دقائق) صاعدة (فتح: ${previousCandleOpen.toFixed(3)}, إغلاق: ${previousCandleClose.toFixed(3)}).`);
                             } else {
                                 bot.sendMessage(chatId, `↔ الشمعة السابقة (5 دقائق) بدون تغيير. لا يوجد اتجاه واضح.`);
